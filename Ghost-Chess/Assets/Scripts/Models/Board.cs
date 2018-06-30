08b6c401aec5690244e92d802b2144880ba5dd0d
Melih <mlhtncc@gmail.com>
2018-06-30 22:35:53 +0300

    Model and Controller Classes are added

---
+++ Ghost-Chess/Assets/Scripts/Models/Board.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Board
+{
+    Tile[,] tiles;
+
+    public Board(int rows, int cols)
+    {
+        tiles = new Tile[rows, cols];
+    }
+}
---
+++ Ghost-Chess/Assets/Scripts/Models/Tile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Tile
+{
+    private int x;
+    private int y;
+
+    public Tile(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
