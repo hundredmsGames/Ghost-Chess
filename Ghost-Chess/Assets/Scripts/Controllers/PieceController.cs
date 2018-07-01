e41f4d3fb60233b32a32913bdea6a25ee6698897
SOFTSAM <sml.ozclk@gmail.com>
2018-07-01 12:52:51 +0300

    controllers

--- Ghost-Chess/Assets/Scripts/Controllers/PieceController.cs
+++ Ghost-Chess/Assets/Scripts/Controllers/PieceController.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class PieceController : MonoBehaviour {
 
+    Dictionary<Piece, GameObject> pieceToGameObjectMap;
+    int pieceCount=16;
 	// Use this for initialization
 	void Start () {
-		
+        pieceToGameObjectMap = new Dictionary<Piece, GameObject>();
+
+        for (int i = 0; i < pieceCount; i++)
+        {
+            Piece piece = new Piece();
+            GameObject pieceGO = new GameObject();
+            pieceGO.AddComponent<SpriteRenderer>();
+            pieceGO.transform.SetParent(this.transform);
+            pieceToGameObjectMap.Add(piece, pieceGO);
+        }
+
 	}
 	
 	// Update is called once per frame
